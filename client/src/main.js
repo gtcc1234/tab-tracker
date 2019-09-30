5de37dc5c6f4693f30e5587e39d16208f08eedf8
Gerald Timothy Chua <gtc@Geralds-MacBook-Pro.local>
2019-09-30 23:33:51 +0800

    Add validation and SQL

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import vuetify from './plugins/vuetify'
+import '@babel/polyfill'
 
 Vue.config.productionTip = false
 
@@ -11,5 +13,6 @@ new Vue({
   el: '#app',
   router,
   components: { App },
+  vuetify,
   template: '<App/>'
 })
